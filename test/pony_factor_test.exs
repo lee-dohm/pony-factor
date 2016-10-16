8ae953692c42c4010b03adfc9c1bff3dedcd40c4
Lee Dohm <lee@lee-dohm.com>
2016-10-16 13:33:46 -0700

    :art:

--- test/pony_factor_test.exs
+++ test/pony_factor_test.exs
@@ -9,6 +9,15 @@ defmodule PonyFactorTest do
     def exit(value), do: value
   end
 
+  setup do
+    {
+      :ok,
+      [
+        list: [{"Name", "Date", "Count"}]
+      ]
+    }
+  end
+
   test "when display is passed an error it issues a shutdown" do
     capture_io(fn ->
       assert PonyFactor.display({:error, "test"}, MockKernel) == {:shutdown, 1}
@@ -21,8 +30,8 @@ defmodule PonyFactorTest do
     assert capture_io(fun) == "test\n"
   end
 
-  test "when display is passed a single list, it displays an Augmented Pony Factor" do
-    fun = fn -> PonyFactor.display([{"Name", "Date", "Count"}]) end
+  test "when display is passed a single list, it displays an Augmented Pony Factor", context do
+    fun = fn -> PonyFactor.display(context[:list]) end
 
     assert capture_io(fun) == """
                               Name\tCount\tDate
@@ -31,8 +40,8 @@ defmodule PonyFactorTest do
                               """
   end
 
-  test "when display is passed a list and a tuple of two numbers, it displays a message and the Pony Factor" do
-    fun = fn -> PonyFactor.display({[{"Name", "Date", "Count"}], {10, 100}}) end
+  test "when display is passed a list and a tuple of two numbers, it displays a message and the Pony Factor", context do
+    fun = fn -> PonyFactor.display({context[:list], {10, 100}}) end
 
     assert capture_io(fun) == """
                               Augmented Pony Factor is undefined: only 5.0% of commits are covered by committers who are still active
